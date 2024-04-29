c5215c89fa4f851255dc12d3b03ffb2e7886d0d4
MBA <MBA@d.c>
2024-04-29 18:14:23 +0300

    Головний клас, який запускає

---
+++ src/main/java/com/example/AttendaceTracker/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.AttendaceTracker;
+
+import com.example.AttendaceTracker.ui.Sign_In_UI;
+
+/**
+ *
+ * @author boiik
+ */
+public class Main {
+    public static void main(String[] argc) {
+        Sign_In_UI startup = new Sign_In_UI();
+        startup.setVisible(true);
+    }
+}
