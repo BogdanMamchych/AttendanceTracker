8b86449d35d3b789e2350cbfc7fb5295f18ace94
MBA <MBA@d.c>
2024-04-30 17:16:05 +0300

    Виправлення входу та додавання функції відміни входу та отримання автентифікаційного коду
    Цей коміт додає деякі нововведення до функції входу, такі як: виконання функції в іншому потоці(завдяки цьому, основний потік, де виконується та функціонує інтерфейс користувача, не зупиняється), автоматичне відкриття сайту Microsoft для введення автентифікаційного коду. Додано функції скасування входу, яка дозволяє зупинити процес входу у випадку, якщо користувач випадково закрив сторінку браузера, браузер або користувач відмовився від входу та зчитування ідентифікаційного коду(довжиною в 9 знаків)
    
    Мамчич Богдан Анатолійович
    30.04.2024

--- src/main/java/com/example/AttendaceTracker/services/Authentication_Service.java
+++ src/main/java/com/example/AttendaceTracker/services/Authentication_Service.java
@@ -4,7 +4,7 @@
  */
 package com.example.AttendaceTracker.services;
 
-import com.example.AttendaceTracker.services.Greetings_Service;
+import com.example.AttendaceTracker.ui.Messages_UI;
 import com.microsoft.aad.msal4j.DeviceCodeFlowParameters;
 import com.microsoft.aad.msal4j.IAuthenticationResult;
 import com.microsoft.aad.msal4j.PublicClientApplication;
@@ -19,76 +19,116 @@ import java.net.*;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-/**
- *
- * @author boiik
- */
 public class Authentication_Service {
+
+    //Клієнтський ідентифікатор
     private static final String clientId = "8cbb4f6d-0b15-4073-8cb0-6e24bacdc73a";
+    
+    //Авторитет для автентифікації через Microsoft
     private static final String authority = "https://login.microsoftonline.com/c603a4af-bdb4-496d-903e-e2b63e0ec56e";
+    
+    //Область обмеження для автентифікації через Microsoft
     private static final Set<String> scopes = Collections.singleton("User.Read");
+    
+    //Об'єкт, що представляє майбутнє значення для результату аутентифікації
     private CompletableFuture<IAuthenticationResult> future;
+    
+    //Токен доступу
     private static String accessToken = "";
     
-    public void signIn(JTextField area, Consumer<Boolean> callback) throws MalformedURLException {
-    PublicClientApplication app = PublicClientApplication.builder(clientId)
-            .authority(authority).build();
+    //Об'єкт для відображення повідомлень користувачу
+    private final Messages_UI messages = new Messages_UI();
 
-    future = app.acquireToken(
-            DeviceCodeFlowParameters.builder(scopes, deviceCodeConsumer -> {
-                String message = deviceCodeConsumer.message();
-                String code = getAuthenticationCode(message);
+    //Функція входу в Outlook;
+    //Етапи виконання функції:
+    //1. Створення екземпляру PublicClientApplication для роботи
+    // з Azure Active Directory;
+    //2. Отримання автентифікаційного коду за допомогою пристрою, 
+    // за допомогою acquireToken;
+    //3. Відображення отриманого коду в вікні за допомогою текстового рядка field;
+    //4. Автоматичне відкриття сайту Microsoft для введення 
+    // автентифікаційного коду за допомогою методу browse;
+    //4.2. У випадку помилки: Виведення повідомлення про помилку за допомогою
+    // messages.showErrorMessage
+    //5. Асинхронне виконання з метою уникнення блокування основного потоку
+    // за допомогою CompletableFuture.runAsync;
+    //У випадку помилки: Виведення повідомлення про помилку за допомогою
+    // messages.showErrorMessage
+    public void signInOutlook(JTextField field, Consumer<Boolean> callback) throws MalformedURLException {
+        
+        //Створення екземпляру PublicClientApplication для роботи
+        // з Azure Active Directory
+        PublicClientApplication app = PublicClientApplication.builder(clientId)
+                .authority(authority).build();
 
-                SwingUtilities.invokeLater(() -> {
-                    area.setText(code);
-                    try {
-                        Desktop.getDesktop().browse(new URI(deviceCodeConsumer.verificationUri()));
-                    } catch (URISyntaxException | IOException e) {
-                        e.printStackTrace();
-                    }
-                });
+        //Отримання автентифікаційного коду за допомогою пристрою, 
+        // за допомогою acquireToken
+        future = app.acquireToken(
+                DeviceCodeFlowParameters.builder(scopes, deviceCodeConsumer -> {
+                    //Отримання повідомлення з кодом
+                    String message = deviceCodeConsumer.message();
+                    //Отримання автентифікаційного коду з повідомлення
+                    String code = getAuthenticationCode(message);
 
-            }).build()
-    );
+                    SwingUtilities.invokeLater(() -> {
+                        //Відправлення автентифікаційного коду до вікна через JTextField
+                        field.setText(code);
+                        try {
+                            //Автоматичне відкриття сайту Microsoft для введення автентифікаційного коду
+                            Desktop.getDesktop().browse(new URI(deviceCodeConsumer.verificationUri()));
+                        } catch (URISyntaxException | IOException e) {
+                            //У разі помилки
+                            messages.showErrorMessage(e.toString());
+                        }
+                    });
 
-    CompletableFuture.runAsync(() -> {
-        try {
-            IAuthenticationResult result = future.get();
-            accessToken = result.accessToken();
-            // Викликаємо callback з результатом
-            callback.accept(true);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            // Викликаємо callback з помилкою
-            callback.accept(false);
-        }
-    });
-}
+                }).build()
+        );
 
+        //Асинхронне виконання з метою уникнення блокування основного потоку
+        CompletableFuture.runAsync(() -> {
+            try {
+                IAuthenticationResult result = future.get();
+                accessToken = result.accessToken();
+                // Викликаємо callback з результатом
+                callback.accept(true);
+            } catch (InterruptedException | ExecutionException e) {
+                //У разі помилки
+                messages.showErrorMessage(e.toString());
+                // Викликаємо callback з помилкою
+                callback.accept(false);
+            }
+        });
+    }
 
-    
-
-    
+    //Фукнція повернення токену доступу
     public String getAccessToken() {
         return accessToken;
     }
 
+    //Функція отримання автентифікаційного коду
     private String getAuthenticationCode(String string) {
-        // Паттерн для пошуку фрагментів, які складаються з 9 великих літер
+        //Паттерн для пошуку фрагментів, які складаються з 9 великих літер(довжина коду)
         Pattern pattern = Pattern.compile("\\b[A-Z0-9]{9}\\b");
 
         // Пошук фрагмента у вихідному рядку
         Matcher matcher = pattern.matcher(string);
         if (matcher.find()) {
+            //Якщо автентифікаційий код знайдено знайдено
             String extractedFragment = matcher.group();
             return extractedFragment;
         } else {
-            System.out.println("Fragment not found in the original string.");
-            return "error";
+            return "Коду немає";
+        }
+    }
+    
+    //Функція скасування виконання входу
+    public void cancelSignIn() {
+        if (future != null && !future.isDone()) {
+            future.cancel(true);
         }
     }
 }
