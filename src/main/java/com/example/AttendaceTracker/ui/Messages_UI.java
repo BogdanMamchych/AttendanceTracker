3e968a0cac92ca1bbae1c362580c646b693a6295
MBA <MBA@d.c>
2024-05-01 13:02:16 +0300

    Реалізація виведення помилки в класі виведення різних повідомлень
    Цей коміт додає функцію виведення повідомлення про помилку в класі виведення різних повідомлень
    
    Мамчич Богдан Анатолійович
    01.05.2024

--- src/main/java/com/example/AttendaceTracker/ui/Messages_UI.java
+++ src/main/java/com/example/AttendaceTracker/ui/Messages_UI.java
@@ -6,12 +6,12 @@ package com.example.AttendaceTracker.ui;
 
 import javax.swing.*;
 
-public class Messages_UI {
+public class Messages_UI extends JFrame{
     //Клас Messages_UI відповідає за виведення на екран різних повідомлень(помилка, успіх і т.д.);
     
     //Функція виведення повідомлення про помилку
     public void showErrorMessage(String message) {
-        JOptionPane.showMessageDialog(parentComponent, message, message, 0);
+        JOptionPane.showMessageDialog(this, message, "Помилка", JOptionPane.ERROR_MESSAGE);
     }
     
 }
