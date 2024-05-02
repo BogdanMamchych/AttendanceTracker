2b55d212437b7e63c6236b67492dd06960604529
MBA <MBA@d.c>
2024-05-02 12:32:22 +0300

    Реалізація бізнес логіки класу для відображення повідомлень в вікні входу в Outlook
    Мамчич Богдан Анатолійович
    02.05.2024

--- src/main/java/com/example/AttendaceTracker/services/Greetings_Service.java
+++ src/main/java/com/example/AttendaceTracker/services/Greetings_Service.java
@@ -4,37 +4,52 @@
  */
 package com.example.AttendaceTracker.services;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 
-/**
- *
- * @author boiik
- */
 public class Greetings_Service {
+    //Клас Greetings_Service відповідає за виведення повідомлення
+    // в вікні входу в Outlook
 
     String youHaveBeenSignedInMessage = "Вхід виконано";
     String greetingsMessage = "Ласкаво просимо";
     String checkMessage = "Перевірте";
-    String checkConnectingMessage = "Підключення";
-    String checkConnectingToInternetMessage = "До інтернету";
+    String checkConnectionMessage = "Підключення";
+    String checkConnectionToInternetMessage = "До інтернету";
 
+    //Функція відображення текстових строк, розташованих вище(greetingsMessage,
+    // checkMessage і т.д.)
     public String greetingShow(String message) {
-        if (message.equals("YouHaveBeenSigned")) {
-            return youHaveBeenSignedInMessage;
-        } else {
-            return greetingsMessage;
-        }
+        return switch (message) {
+            case "YouHaveBeenSigned" -> youHaveBeenSignedInMessage;
+            case "greetings" -> greetingsMessage;
+            case "checkConnection" -> checkConnectionMessage;
+            case "checkConnectionToInternet" -> checkConnectionToInternetMessage;
+            default -> checkMessage;
+        };
     }
 
+    //Виведення повідомлення привітання у разі успішного входу
     public void greeting(JTextField field) {
         try {
             field.setText(greetingShow("YouHaveBeenSigned"));
             TimeUnit.SECONDS.sleep(3);
             field.setText(greetingShow("greetings"));
             TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //Виведення повідомлення про відсутність з'єднання у разі відсутності інтернету
+    public void connectingError(JTextField field) {
+        try {
+            field.setText(greetingShow("check"));
+            TimeUnit.SECONDS.sleep(2);
+            field.setText(greetingShow("checkConnection"));
+            TimeUnit.SECONDS.sleep(2);
+            field.setText(greetingShow("checkConnectionToInternet"));
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
