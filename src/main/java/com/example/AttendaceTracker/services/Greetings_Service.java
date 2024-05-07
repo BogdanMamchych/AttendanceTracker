6376d2049f7baee38800e40f85a1af930d73d14e
MBA <MBA@d.c>
2024-05-07 10:00:09 +0300

    Зміни в функції в класі виведення повідомлення в вікно входу в Outlook
    Цей коміт додає деякі зміни в функції, а саме: відмова від функції виведення помилки підключення, видалення деяких повідомлень та зменшення часу відображення повідомленння
    
    Мамчич Богдан
    07.05.2024

--- src/main/java/com/example/AttendaceTracker/services/Greetings_Service.java
+++ src/main/java/com/example/AttendaceTracker/services/Greetings_Service.java
@@ -4,54 +4,38 @@
  */
 package com.example.AttendaceTracker.services;
 
+import com.example.AttendaceTracker.ui.Messages_UI;
 import java.util.concurrent.TimeUnit;
 import javax.swing.JTextField;
 
-public class Greetings_Service {
+public class Greetings_Service{
     //Клас Greetings_Service відповідає за виведення повідомлення
     // в вікні входу в Outlook
 
     String youHaveBeenSignedInMessage = "Вхід виконано";
     String greetingsMessage = "Ласкаво просимо";
-    String checkMessage = "Перевірте";
-    String checkConnectionMessage = "Підключення";
-    String checkConnectionToInternetMessage = "До інтернету";
+
 
     //Функція відображення текстових строк, розташованих вище(greetingsMessage,
     // checkMessage і т.д.)
-    public String greetingShow(String message) {
-        return switch (message) {
-            case "YouHaveBeenSigned" -> youHaveBeenSignedInMessage;
-            case "greetings" -> greetingsMessage;
-            case "checkConnection" -> checkConnectionMessage;
-            case "checkConnectionToInternet" -> checkConnectionToInternetMessage;
-            default -> checkMessage;
-        };
+    private String greetingShow(String message) {
+        if(message.equals("YouHaveBeenSigned")) { 
+            return youHaveBeenSignedInMessage;
+        } else {
+            return greetingsMessage;
+        }
     }
 
     //Виведення повідомлення привітання у разі успішного входу
+    
     public void greeting(JTextField field) {
         try {
             field.setText(greetingShow("YouHaveBeenSigned"));
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.SECONDS.sleep(2);
             field.setText(greetingShow("greetings"));
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    //Виведення повідомлення про відсутність з'єднання у разі відсутності інтернету
-    public void connectingError(JTextField field) {
-        try {
-            field.setText(greetingShow("check"));
-            TimeUnit.SECONDS.sleep(2);
-            field.setText(greetingShow("checkConnection"));
-            TimeUnit.SECONDS.sleep(2);
-            field.setText(greetingShow("checkConnectionToInternet"));
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Messages_UI.showErrorMessage(e.toString());
         }
     }
 
