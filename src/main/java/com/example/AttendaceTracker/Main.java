55e22647f51374b494165ace88364f7bab43dcbf
MBA <MBA@d.c>
2024-05-07 08:39:50 +0300

    Добавлено виконання функції зчитування конфігурацій з файлу config.properties
    Мамчич Богдан
    07.05.2024

--- src/main/java/com/example/AttendaceTracker/Main.java
+++ src/main/java/com/example/AttendaceTracker/Main.java
@@ -4,15 +4,17 @@
  */
 package com.example.AttendaceTracker;
 
+import com.example.AttendaceTracker.services.Config_Service;
 import com.example.AttendaceTracker.ui.Sign_In_UI;
 
-/**
- *
- * @author boiik
- */
+
 public class Main {
+    //Клас Main - стартер програми
     public static void main(String[] argc) {
         Sign_In_UI startup = new Sign_In_UI();
+        //Відкриття головного меню
         startup.setVisible(true);
+        //Зчитування конфігурацій з файлу config.properties
+        Config_Service.getProperties();
     }
 }
