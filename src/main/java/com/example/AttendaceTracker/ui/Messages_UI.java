3bbe92af02d67d676fd1239ed2bba4b8944ec308
MBA <MBA@d.c>
2024-05-07 08:43:04 +0300

    Зміна функції відображення повідомлень про помилку на статичну
    Цей коміт додає одне нововведення в функцію зчитування про помилку, а саме: перетворення функції на статичну, завдяки чому функцію можна викликати без створення об'єкту класу Messages_UI
    
    Мамчич Богдан
    07.05.2024

--- src/main/java/com/example/AttendaceTracker/ui/Messages_UI.java
+++ src/main/java/com/example/AttendaceTracker/ui/Messages_UI.java
@@ -4,14 +4,15 @@
  */
 package com.example.AttendaceTracker.ui;
 
-import javax.swing.*;
+import java.awt.Component;
+import javax.swing.JOptionPane;
 
-public class Messages_UI extends JFrame{
+public class Messages_UI{
     //Клас Messages_UI відповідає за виведення на екран різних повідомлень(помилка, успіх і т.д.);
     
     //Функція виведення повідомлення про помилку
-    public void showErrorMessage(String message) {
-        JOptionPane.showMessageDialog(this, message, "Помилка", JOptionPane.ERROR_MESSAGE);
+    public static void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(null, message, "Помилка", JOptionPane.ERROR_MESSAGE);
     }
     
 }
