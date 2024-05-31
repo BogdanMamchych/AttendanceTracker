bcc3cc1718fca3e669fbc262f928679aa5165ded
MBA <MBA@d.c>
2024-05-31 16:37:54 +0300

    Зміни та нововведення в класі Subject_Service
    Цей коміт додає деякі нововведення в клас Subject_Serivce, а саме: Зміна назви класу на Subject_Service, добавлення єдиного екземпляру класу Subject_Model(в цьому класі зберігатимуться список назв предметів) за шаблоном Singleton. Добавлення функції toModel(для конвертування даних до Subject_Model для подальшого зберігання у зв'язку, зі змінами в Subject_DTO)
    
    Мамчич Богдан
    31.05.2024

--- src/main/java/com/example/AttendaceTracker/services/Subject_Service.java
+++ src/main/java/com/example/AttendaceTracker/services/Subject_Service.java
@@ -4,59 +4,76 @@
  */
 package com.example.AttendaceTracker.services;
 
-import com.example.AttendaceTracker.dto.Subject_List_DTO;
-import com.example.AttendaceTracker.model.Subject_List_Model;
-import com.example.AttendaceTracker.model.User_Model;
+import com.example.AttendaceTracker.repository.Database_Repository;
+import com.example.AttendaceTracker.dto.Subject_DTO;
+import com.example.AttendaceTracker.model.Instructor_Model;
+import com.example.AttendaceTracker.model.Subject_Model;
 import java.util.ArrayList;
 
-
 public class Subject_Service {
-    //Клас Subject_Service відповідає за дії над списком предметів
-    
-    //Функція отримання даних предметів
-    public static String receive(ArrayList<String> parameters, ArrayList<Object> values) {
-        //Отримання даних предметів
-        String result = Database_Service.retreiveDataByParameters(parameters, values, "*", "Subject");
-        //Якщо дані отримано успішно
+    //Клас Subject_Service відповідає за дії над списком предметів з бази даних
+
+    //Посилання на модель предметів
+    private Subject_Model subjectModel = Subject_Model.getInstance();
+    //Дані з таблиці Subject
+    private ArrayList<ArrayList<Object>> subjectRows;
+    //Стовбці з таблиці Subject
+    private ArrayList<String> subjectColumns;
+   
+    // Функція отримання даних предметів
+    private String receive(ArrayList<String> parameters, ArrayList<Object> values) {
+        // Отримання даних предметів
+        String result = Database_Repository.retrieveDataByParameters(parameters, values, "*", "Subject",
+                subjectRows, subjectColumns);
+        // Якщо дані отримано успішно
         if (result.equals("successfull")) {
-            //Вилучення отриманих даних
-            String result_getData = Database_Service.getData(Subject_List_Model.getSubjectDataList());
-            //Вилучення назв стовбців таблиці дисципліни
-            String result_getColumn = Database_Service.getColumns(Subject_List_Model.getSubjectColumnsList());
-            //Якшо дані вилучені успішно
-            if (result_getData.equals("successfull") && result_getColumn.equals("successfull")) {
-                return "successfull";
-            }
+            return "successfull";
         }
         return result;
     }
     
-    //Функція-стартер функції отримання даних предметів
-    public static String recieveSubjectData() {
-        //Формування списку назв параметрів
-        ArrayList<String> parameters = new ArrayList();
+    public boolean isEmpty() {
+        return subjectModel.getSubjects().isEmpty();
+    }
+
+    // Функція-стартер функції отримання даних предметів
+    public String fetchSubjectData() {
+        // Формування списку назв параметрів
+        ArrayList<String> parameters = new ArrayList<>();
         parameters.add("instructor_ID");
-        //Формування списку параметрів
-        ArrayList<Object> values = new ArrayList();
-        values.add(User_Model.getId());
-        //Виконання функції отримання даних предметів
+        // Формування списку параметрів
+        ArrayList<Object> values = new ArrayList<>();
+        Instructor_Model model = Instructor_Model.getInstance();
+        values.add(model.getId());
+        // Виконання функції отримання даних предметів
         return receive(parameters, values);
+    }
+
+    // Конвертація даних предметів в model для Зберігання
+    public void toModel() {
+        subjectModel = Subject_Model.getInstance();
+        // Отримання даних зі списку предметів
+        for (ArrayList<Object> rowData : subjectRows) {
+            String subject = (String) rowData.get(findColumn("name"));
+            subjectModel.getSubjects().add(subject);
+        }
     }
     
     //Конвертація даних предметів в DTO для подальшого використання
-    public static void toDTO() {
-        ArrayList<String> subjects = new ArrayList<>();
-        //Отримання даних зі списку предметів
-        for(ArrayList<Object> rowData : Subject_List_Model.getSubjectDataList()) {
-            String subject = (String) rowData.get(Subject_List_Model.findColumn("name"));
-            subjects.add(subject);
-        }
-        //Внесення даних в DTO
-        Subject_List_DTO.setList(subjects);
+    public Subject_DTO toDTO() {
+        Subject_DTO dto = new Subject_DTO();
+        dto.setList(subjectModel.getSubjects());
+        return dto;
     }
-    
-    //Функція ініціалізації списків в класі Subject_List_Model
-    public static void init() {
-        Subject_List_Model.init();
+
+    // Функція пошуку стовбця
+    public int findColumn(String columnName) {
+        for (int i = 0; i < subjectColumns.size(); i++) {
+            // Якщо даний стовбець є в списку
+            if (subjectColumns.get(i).equals(columnName)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
