71c82463d5b0c5383a87a6668190a77328191c88
MBA <MBA@d.c>
2024-05-31 20:28:02 +0300

    Добавлення нових та зміна деяких існуючих коментарів в класі Schedule_List_Model
    Мамчич Богдан
    31.05.2024

--- src/main/java/com/example/AttendaceTracker/model/Schedule_List_Model.java
+++ src/main/java/com/example/AttendaceTracker/model/Schedule_List_Model.java
@@ -7,33 +7,40 @@ package com.example.AttendaceTracker.model;
 import java.util.ArrayList;
 
 public class Schedule_List_Model {
-    //Клас Schedule_List_Model презентує дані з таблиці ScheduleEvent(Урок)
+    //Клас Schedule_List_Model відповідає за зберігання даних про уроки на сьогодні(1 день)
 
     //Дані з таблиці ScheduleEvent
-    private static ArrayList<ArrayList<Object>> scheduleData;
+    private static ArrayList<ArrayList<Object>> scheduleRows;
     
     //Назви стовбців з таблиці ScheduleEvent 
     private static ArrayList<String> scheduleColumns;
 
+    //Ініціалізація списків для зберігання даних та ствбців з таблиці ScheduleEvent
     public static void initialization() {
-        if (scheduleData == null) {
-            scheduleData = new ArrayList<>();
+        //Якщо список даних порожній
+        if (scheduleRows == null) {
+            scheduleRows = new ArrayList<>();
         }
+        //Якщо список назв стовбців порожній
         if (scheduleColumns == null) {
             scheduleColumns = new ArrayList<>();
         }
     }
 
-    public static ArrayList<ArrayList<Object>> getScheduleData() {
-        return scheduleData;
+    //Функція отримання даних з таблиці ScheduleEvent
+    public static ArrayList<ArrayList<Object>> getScheduleRows() {
+        return scheduleRows;
     }
     
+    //Функція отримання назв стовбців з таблиці ScheduleEvent
     public static ArrayList<String> getScheduleColumns() {
         return scheduleColumns;
     }
     
+    //Функція знаходження стовбця
     public static int findColumn(String columnName) {
         for (int i = 0; i < scheduleColumns.size(); i++) {
+            //Якщо стовбець знайдено
             if (scheduleColumns.get(i).equals(columnName)) {
                 return i;
             }
